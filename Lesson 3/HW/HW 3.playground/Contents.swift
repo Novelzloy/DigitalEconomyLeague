ac992425747a2d0c5d2efe6eba88b39ea9ccea7c
Roman Naumov <55456532+Novelzloy@users.noreply.github.com>
2022-03-27 19:22:19 +0300

    Add files via upload

---
+++ Lesson 3/HW/HW 3.playground/Contents.swift
@@ -0,0 +1,70 @@
+
+//
+//  SRPViewController.swift
+//  SOLIDDRYKISS
+//
+
+//
+import UIKit
+import Foundation
+
+enum CaloriesCalculatorType {
+    case mifflin
+    case harrisBenedict
+}
+
+enum ProportionType {
+    case paleo
+    case keto
+}
+
+struct Nutrients {
+    let carbs: Int
+    let fats: Int
+    let proteins: Int
+}
+
+protocol CaloriesCalculation {
+    func calculationProrortion(proportionType: ProportionType) -> Nutrients
+}
+
+protocol ProportionCalculation {
+    func calculationTargetColorias(caloriesCalculationType: CaloriesCalculatorType) -> Int
+}
+
+struct Calories: CaloriesCalculation {
+    func calculationProrortion(proportionType: ProportionType) -> Nutrients {
+        var nutrients: Nutrients
+        switch proportionType {
+        case .paleo:
+            nutrients = Nutrients(carbs: 4, fats: 10, proteins: 8)
+        default:
+            nutrients = Nutrients(carbs: 6, fats: 1, proteins: 20)
+        }
+        return nutrients
+    }
+}
+
+struct Proportion: ProportionCalculation {
+    func calculationTargetColorias(caloriesCalculationType: CaloriesCalculatorType) -> Int {
+        var targetCalories: Int
+        switch caloriesCalculationType {
+        case .harrisBenedict:
+            targetCalories = 1200
+        default:
+            targetCalories = 1400
+        }
+        return targetCalories
+    }
+}
+
+var calories = Calories()
+calories.calculationProrortion(proportionType: .paleo)
+
+var proportion = Proportion()
+proportion.calculationTargetColorias(caloriesCalculationType: .harrisBenedict)
+
+class SRPViewController {
+    var calories = Calories()
+    var proportion = Proportion()
+}
