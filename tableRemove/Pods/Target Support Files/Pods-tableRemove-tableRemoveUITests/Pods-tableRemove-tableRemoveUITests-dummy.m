af327d6f4d07d07874ef119f42fb6bc6860a14a1
Roman Naumov <55456532+Novelzloy@users.noreply.github.com>
2022-03-27 16:29:30 +0300

    Add files via upload

---
+++ tableRemove/Pods/Target Support Files/Pods-tableRemove-tableRemoveUITests/Pods-tableRemove-tableRemoveUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tableRemove_tableRemoveUITests : NSObject
+@end
+@implementation PodsDummy_Pods_tableRemove_tableRemoveUITests
+@end
---
+++ tableRemove/tableRemove/Model/Model.swift
@@ -0,0 +1,37 @@
+//
+//  Model.swift
+//  tableRemove
+//
+//  Created by Роман on 23.03.2022.
+//
+
+import Foundation
+
+struct Item {
+    var nameItem: String
+    var itemToList: [Food]
+
+}
+
+struct Food {
+    var nameFood: String
+}
+
+var itemList = [
+    Item(nameItem: "Fruit", itemToList: [Food(nameFood: "Apple"),
+                                         Food(nameFood: "Avocado"),
+                                         Food(nameFood: "Apricot"),
+                                         Food(nameFood: "Grapes"),]),
+    Item(nameItem: "Vegetables", itemToList: [Food(nameFood: "Squash"),
+                                              Food(nameFood: "Cabbage"),
+                                              Food(nameFood: "Potato"),
+                                              Food(nameFood: "Onion"),]),
+    Item(nameItem: "Fish", itemToList: [Food(nameFood: "Carp"),
+                                        Food(nameFood: "Vobla"),
+                                        Food(nameFood: "Peled"),
+                                        Food(nameFood: "Omul"),]),
+    Item(nameItem: "Meat", itemToList: [Food(nameFood: "Beef"),
+                                        Food(nameFood: "Duck"),
+                                        Food(nameFood: "Pork"),
+                                        Food(nameFood: "Lamb"),]),
+]
