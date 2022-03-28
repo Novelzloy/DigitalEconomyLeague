3cccc95557fb990296e59162b79bbbd25e08c8d0
Roman Naumov <55456532+Novelzloy@users.noreply.github.com>
2022-03-28 19:53:40 +0300

    Delete Lesson 8 directory

--- Lesson 8/HW/tableRemove/Pods/Target Support Files/Pods-tableRemove/Pods-tableRemove-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tableRemove : NSObject
-@end
-@implementation PodsDummy_Pods_tableRemove
-@end
--- Lesson 8/HW/tableRemove/Pods/Target Support Files/Pods-tableRemoveTests/Pods-tableRemoveTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tableRemoveTests : NSObject
-@end
-@implementation PodsDummy_Pods_tableRemoveTests
-@end
